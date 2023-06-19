26085195f4d9e3a2f04965987e4899f113f959fc
Waliston <goneisgone@hotmail.com>
2023-06-19 19:37:00 -0300

    JS Advanced Concepts - Fundation II - Lexical Environment

---
+++ JavaScript-Foundation-II/Lexical-Environment.js
@@ -0,0 +1,6 @@
+/**
+ * In javascript our Lexical scope(available data +
+ * variables where the function wes defined)
+ *  Determines our available varaibles.
+ * Note where the function is called (Dynamic scope)
+ */
