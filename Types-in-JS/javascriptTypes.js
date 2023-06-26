8da9e542e29941690ebf56589ca7683a7af3eacb
Waliston <goneisgone@hotmail.com>
2023-06-26 12:09:25 -0300

    JS ADvanced Concepts - JS Types

---
+++ Types-in-JS/javascriptTypes.js
@@ -0,0 +1,19 @@
+//Types in JS we jave 7
+
+// Numbers => 1, 1.1
+// Booleran => true or false (0 or 1)
+// String => "String"
+// Undefined
+// Null
+// Symbol('just me)
+// Objects => {}
+
+// We can discover using the operator TypeOf, example:
+console.log(typeof 5);
+console.log(typeof true);
+console.log(typeof 'sssss');
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof Symbol('its me mario'));
+console.log(typeof {});
+
