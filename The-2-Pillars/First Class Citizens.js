0f8c5718acccf6ad7aa1dceac1bc94126ce49a8a
Waliston <goneisgone@hotmail.com>
2023-07-10 18:18:00 -0300

    Function are fist class citizens in JS

---
+++ The-2-Pillars/First Class Citizens.js
@@ -0,0 +1,22 @@
+//Function are fist class citizens in JS
+//1 
+var stuff = function(){}
+
+
+//2 
+function a(fn) {
+    fn()
+}
+
+a(function(){
+    console.log('hi there');
+})
+
+//3
+function b() {
+    return function c() {
+        console.log("bye");
+    }
+}
+
+b()
