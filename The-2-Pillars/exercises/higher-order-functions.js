2776a7bffa58555018f2fbb99df7a888402b87cd
Waliston <goneisgone@hotmail.com>
2023-07-11 17:06:28 -0300

    Higher order functions Exercise

---
+++ The-2-Pillars/exercises/higher-order-functions.js
@@ -0,0 +1,13 @@
+const num1 = (num) => {
+    return num;
+} 
+
+const num2 = (num) => {
+    return num;
+}
+
+const multiply = (n1,n2) => {
+    return console.log(`the result of ${num1(n1)} X ${num2(n2)} is ${n1*n2}`);
+} 
+
+multiply(5,8)
