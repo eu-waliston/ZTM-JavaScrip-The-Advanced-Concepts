65df7072fe9923493ff8e8cda6edcadaa0417980
Waliston <goneisgone@hotmail.com>
2023-07-20 11:45:05 -0300

    JS advances concepts >> Functional programming - Imperative vs Declarative

---
+++ Functional-Programming/ImperativevsDeclarative.js
@@ -0,0 +1,14 @@
+//Imperative vs Declarative
+
+//Imperative
+for(let i = 0; i < 11; i++) {
+    console.log(i);
+}
+
+
+//Declarative
+console.log("----------------");
+let arr = [1,2,3]
+arr.forEach(item => {
+    console.log(item);
+})
