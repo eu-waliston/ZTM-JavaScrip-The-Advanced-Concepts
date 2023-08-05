a2aea47d99b68ef270105e828b08b4596dc9a43b
Waliston <goneisgone@hotmail.com>
2023-08-05 14:18:31 -0300

    React Blog > Login, Register  and Recover components

---
+++ Data-Structures/Hash/Hash-Collisions.js
@@ -0,0 +1,13 @@
+//Hash Collisions
+let user = {
+  age: 54,
+  name: "Kylie",
+  magic: true,
+  scream: function () {
+    console.log("ahhhhhhh!");
+  },
+};
+
+console.log(user.age);
+console.log(user.spell = 'abra kadabra!');
+console.log(user);
---
+++ Data-Structures/Hash/Hash-Tables-in-Different-Languages.js
@@ -0,0 +1,1 @@
+// Hash Tables in Different Languages
